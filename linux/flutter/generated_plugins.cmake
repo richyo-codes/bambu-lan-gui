12d1661e77a04439fd7d20fb0ab77c041c609d77
Richard Young <code@richyoung.ca>
2025-09-11 19:58:33 -0400

    updated plugins

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   file_saver
+  flutter_secure_storage_linux
   media_kit_libs_linux
   media_kit_video
   volume_controller
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   file_saver
+  flutter_secure_storage_windows
   media_kit_libs_windows_video
   media_kit_video
   volume_controller
